4ecee838546b45dc44e0d7010905287712dfea53
Antonio Macías <antmacfer1@alum.us.es>
2025-07-15 13:35:49 +0200

    ci: small fix to check ci/cd

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,7 +2,6 @@ import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
 export default createMiddleware({
-  // Ensure the localePrefix is set to 'always' to enforce locale prefixes in URLs
   localePrefix: 'always',
   // Use the routing configuration from your i18n setup
   locales: routing.locales,
