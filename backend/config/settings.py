0b59c01d0dd63d413ae1dc287bedc78ab6463648
Antonio Macías <antmacfer1@alum.us.es>
2025-07-16 11:56:26 +0200

    feat: disable debug mode in production settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i7k_@04j6#xklqtt$qt3kgnjm@qx2^cj(dkc23)n&@g#=r#b3v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['136.144.243.1', 'http://ordinaly.duckdns.org']
 
