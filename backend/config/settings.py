1e8226ca5603524cfffe6317be948c2f0f153c69
Antonio Macías <antmacfer1@alum.us.es>
2025-07-17 09:27:40 +0200

    feat: add dark mode support and slider component

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i7k_@04j6#xklqtt$qt3kgnjm@qx2^cj(dkc23)n&@g#=r#b3v'
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
