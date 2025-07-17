d0e71fdd9ab1e3cb1900abf8856f41f571482407
Antonio Macías <antmacfer1@alum.us.es>
2025-07-17 09:32:28 +0200

    ci: update ci workflow to create daily PRs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['136.144.243.11', 'http://ordinaly.duckdns.org', 'localhost', '127.0.0.1']
 
