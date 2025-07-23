b614acd8b16489a7a8b4ebebc9921359f9c59a52
Antonio Macías <antmacfer1@alum.us.es>
2025-07-23 14:04:50 +0200

    feat: add user authentication, permission checks and unit tests

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home),  # main page view to test api connection
+    path('courses/', include('courses.urls')),
+    path('terms/', include('terms.urls')),
+    path('users/', include('users.urls')),
+    path('services/', include('services.urls')),
 ]
