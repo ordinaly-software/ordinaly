328ff6e281aa8842cb852c1d1d6a929e948b4fcd
IvanDAM-mg <ivan.moreno.giles.alu@iesjulioverne.es>
2026-04-08 12:13:24 +0200

    fix button contact and color

--- frontend/src/app/[locale]/inteligencia-artificial-sevilla/page.client.tsx
+++ frontend/src/app/[locale]/inteligencia-artificial-sevilla/page.client.tsx
@@ -114,7 +114,7 @@ export default function InteligenciaArtificialSevilla() {
 
       {/* WHATSAPP BUTTON */}
       <a
-        href="https://wa.me/346XXXXXXXXX"
+        href="https://wa.me/34626270806"
         target="_blank"
         rel="noopener noreferrer"
         className="
@@ -134,7 +134,7 @@ export default function InteligenciaArtificialSevilla() {
           shadow-xl
           transition
         "
-        style={{ backgroundColor: "#e4572e" }}
+        style={{ backgroundColor: "#d97757" }}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -145,7 +145,7 @@ export default function InteligenciaArtificialSevilla() {
           <path d="M20.52 3.48A11.8 11.8 0 0 0 12 0C5.37 0 0 5.37 0 12a11.9 11.9 0 0 0 1.64 6L0 24l6.26-1.64A11.9 11.9 0 0 0 12 24c6.63 0 12-5.37 12-12 0-3.2-1.25-6.21-3.48-8.52zM12 22a9.9 9.9 0 0 1-5.06-1.39l-.36-.21-3.72.98 1-3.63-.24-.37A9.9 9.9 0 0 1 2 12c0-5.52 4.48-10 10-10 2.67 0 5.18 1.04 7.07 2.93A9.9 9.9 0 0 1 22 12c0 5.52-4.48 10-10 10zm5.13-7.47c-.28-.14-1.65-.82-1.9-.91-.25-.1-.43-.14-.62.14-.18.28-.71.91-.87 1.1-.16.18-.32.2-.6.07-.28-.14-1.18-.43-2.25-1.38-.83-.74-1.39-1.65-1.55-1.93-.16-.28-.02-.43.12-.57.12-.12.28-.32.43-.48.14-.16.18-.28.28-.46.1-.18.05-.34-.02-.48-.07-.14-.62-1.5-.85-2.06-.22-.53-.45-.46-.62-.46h-.53c-.18 0-.48.07-.73.34-.25.28-.96.94-.96 2.28 0 1.34.98 2.63 1.12 2.81.14.18 1.93 2.95 4.68 4.14.65.28 1.16.45 1.55.58.65.2 1.24.17 1.7.1.52-.08 1.65-.67 1.88-1.32.23-.65.23-1.2.16-1.32-.07-.12-.25-.2-.53-.34z" />
         </svg>
 
-        <span>Contáctanos</span>
+        <span>{content.contactUs}</span>
       </a>
 
       <Footer />
